21494bfb1e584ffcbebc8d30a924db57ca6a139d
shauryacoding <82760764+shauryacoding@users.noreply.github.com>
2021-08-13 19:19:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ canvas =document.getElementById("myCanvas");
 ctx=canvas.getContext("2d");
 color="red";
 ctx.beginPath();
-ctx.strokeStyle=color;
+ctx.strokeStyle=color; 
 ctx.lineWidth=2;
 ctx.arc(200,200,50,0,2*Math.PI);
 ctx.stroke();
